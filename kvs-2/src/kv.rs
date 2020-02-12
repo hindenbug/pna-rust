15d6e9e62a01ab18f4f9a7a266ba3a012fe9b9e9
hindenbug <manoj.mk27@gmail.com>
2020-02-12 23:26:11 +0100

    Store log pointers in the index

--- kvs-2/src/kv.rs
+++ kvs-2/src/kv.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, HashMap};
 use std::env::current_dir;
 use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader, Error, Write};
+use std::io::{self, BufRead, BufReader, Error, Read, Seek, SeekFrom, Write};
 use std::path::PathBuf;
 
 /// The `KvStore` stores string key/value pairs.
@@ -27,7 +27,7 @@ use std::path::PathBuf;
 pub struct KvStore {
     path: PathBuf,
     log: File,
-    map: BTreeMap<String, String>,
+    map: BTreeMap<String, u64>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -47,43 +47,25 @@ impl KvStore {
     // TIL impl Trait
     pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
         let path = path.into();
-        let mut map: BTreeMap<String, String> = BTreeMap::new();
+        let map: BTreeMap<String, u64> = BTreeMap::new();
         fs::create_dir_all(&path)?;
-        let mut log = std::fs::OpenOptions::new()
+        let log = std::fs::OpenOptions::new()
             .create(true)
             .read(true)
             .write(true)
             .append(true)
             .open(&path.join("log.data"))?;
 
-        let reader = BufReader::new(File::open(&path.join("log.data"))?);
-        let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
-
-        while let Some(cmd) = stream.next() {
-            match cmd {
-                Ok(Command {
-                    cmd: CommandType::Set,
-                    key: key,
-                    value: Some(value),
-                }) => {
-                    map.insert(key, value);
-                }
-                Ok(Command {
-                    cmd: CommandType::Rm,
-                    key: key,
-                    value: None,
-                }) => {
-                    map.remove(&key);
-                }
-                _ => panic!(),
-            }
-        }
-
-        Ok(KvStore {
+        // Initialize KvStore
+        let mut store = KvStore {
             path: path,
             log: log,
             map: map,
-        })
+        };
+
+        // Load from log file
+        store.load_from_log()?;
+        Ok(store)
     }
 
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
@@ -92,23 +74,29 @@ impl KvStore {
             key: key.clone(),
             value: Some(value.clone()),
         };
+        let current_log_file_offset = self.log.seek(SeekFrom::End(0))?;
         // encoding before writing to log
         serde_json::to_writer(&mut self.log, &command);
         self.log.flush()?;
-        self.map.insert(key, value);
+        self.map.insert(key, current_log_file_offset);
         Ok(())
     }
 
-    pub fn get(&self, key: String) -> Result<Option<String>> {
-        Ok(self.map.get(&key).cloned())
-        // let stored_value = self.map.get(&key);
-        // match stored_value {
-        //     Some(value) => return Ok(Some(value.to_string())),
-        //     None => return Ok(None),
-        // };
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        // println!("{:?}", key);
+        // println!("{:?}", self.map);
+        if let Some(offset) = self.map.get(&key).cloned() {
+            &self.log.seek(SeekFrom::Start(offset));
+            let mut de = serde_json::Deserializer::from_reader(&self.log);
+            let cmd: Command = serde::de::Deserialize::deserialize(&mut de)?;
+            Ok(cmd.value)
+        } else {
+            Ok(None)
+        }
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
+        self.log.seek(SeekFrom::End(0))?;
         if self.map.get(&key).is_none() {
             return Err(KvsError::KeyNotFound);
         }
@@ -119,7 +107,37 @@ impl KvStore {
         };
         // encoding before writing to log
         serde_json::to_writer(&mut self.log, &command);
+        self.log.flush()?;
         self.map.remove(&key);
+        Ok(())
+    }
+
+    fn load_from_log(&mut self) -> Result<()> {
+        let mut reader = BufReader::new(File::open(&self.path.join("log.data"))?);
+        let mut offset = reader.seek(SeekFrom::Start(0))?;
+        let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
+
+        while let Some(cmd) = stream.next() {
+            let new_offset = stream.byte_offset() as u64;
+            match cmd {
+                Ok(Command {
+                    cmd: CommandType::Set,
+                    key: key,
+                    value: _value,
+                }) => {
+                    self.map.insert(key, offset);
+                }
+                Ok(Command {
+                    cmd: CommandType::Rm,
+                    key: key,
+                    value: None,
+                }) => {
+                    self.map.remove(&key);
+                }
+                _ => panic!(),
+            }
+            offset = new_offset;
+        }
         Ok(())
     }
 }
