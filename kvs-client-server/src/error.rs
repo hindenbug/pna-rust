1e581d56cf822a9b472168386d4879cbab6800d5
hindenbug <manoj.mk27@gmail.com>
2020-02-15 21:30:38 +0100

    [Project 3] Add setup, tests for client-server KVS

---
+++ kvs-client-server/src/error.rs
@@ -0,0 +1,30 @@
+use failure::Fail;
+use std::io;
+
+/// Error type.
+#[derive(Fail, Debug)]
+pub enum KvsError {
+    #[fail(display = "{}", _0)]
+    Io(#[cause] io::Error),
+    /// Removing non-existent key error.
+    #[fail(display = "Key not found")]
+    KeyNotFound,
+    /// Serialization or deserialization error.
+    #[fail(display = "{}", _0)]
+    Serde(#[cause] serde_json::Error),
+}
+
+impl From<io::Error> for KvsError {
+    fn from(err: io::Error) -> KvsError {
+        KvsError::Io(err)
+    }
+}
+
+impl From<serde_json::Error> for KvsError {
+    fn from(err: serde_json::Error) -> KvsError {
+        KvsError::Serde(err)
+    }
+}
+
+/// Result type for kvs.
+pub type Result<T> = std::result::Result<T, KvsError>;
