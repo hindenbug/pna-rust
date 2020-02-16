7d72f8a9790dbb36c24aadfc645d2f6f2a877297
hindenbug <manoj.mk27@gmail.com>
2020-02-16 01:49:51 +0100

    Update kvs-client and kvs-server cli options

--- kvs-client-server/src/bin/kvs-client.rs
+++ kvs-client-server/src/bin/kvs-client.rs
@@ -1,11 +1,32 @@
+//! The kvs-client executable supports the following command line arguments:
+//!
+//!     kvs-client set <KEY> <VALUE> [--addr IP-PORT]
+//!
+//!     Set the value of a string key to a string.
+//!     --addr accepts an IP address, either v4 or v6, and a port number, with the format IP:PORT. If --addr is not specified then connect on 127.0.0.1:4000.
+//!     Print an error and return a non-zero exit code on server error, or if IP-PORT does not parse as an address.
+//!
+//!     kvs-client get <KEY> [--addr IP-PORT]
+//!     Get the string value of a given string key.
+//!     --addr accepts an IP address, either v4 or v6, and a port number, with the format IP:PORT. If --addr is not specified then connect on 127.0.0.1:4000.
+//!     Print an error and return a non-zero exit code on server error, or if IP-PORT does not parse as an address.
+//!
+//!     kvs-client rm <KEY> [--addr IP-PORT]
+//!     Remove a given string key.
+//!     --addr accepts an IP address, either v4 or v6, and a port number, with the format IP:PORT. If --addr is not specified then connect on 127.0.0.1:4000.
+//!     Print an error and return a non-zero exit code on server error, or if IP-PORT does not parse as an address. A "key not found" is also treated as an error in the "rm" command.
+//!
+//!     kvs-client -V
+//!     Print the version.
+//! All error messages should be printed to stderr.
+
 use clap::{App, Arg, SubCommand};
-use kvs::{KvStore, Result};
-use std::env::current_dir;
+use kvs::Result;
+use std::net::TcpStream;
 use std::process::exit;
 
 fn main() -> Result<()> {
-    let mut store = KvStore::open(current_dir()?)?;
-    let matches = App::new(env!("CARGO_PKG_NAME"))
+    let matches = App::new("kvs-client")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
@@ -23,6 +44,14 @@ fn main() -> Result<()> {
                         .help("The value")
                         .required(true)
                         .index(2),
+                )
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .help("address to connect to server")
+                        .takes_value(true)
+                        .value_name("IP:PORT")
+                        .required(false),
                 ),
         )
         .subcommand(
@@ -33,45 +62,51 @@ fn main() -> Result<()> {
                         .help("Key name")
                         .required(true)
                         .index(1),
+                )
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .help("address to connect to server")
+                        .takes_value(true)
+                        .value_name("IP:PORT")
+                        .required(false),
                 ),
         )
         .subcommand(
-            SubCommand::with_name("rm").about("Remove a given key").arg(
-                Arg::with_name("KEY")
-                    .help("Key name")
-                    .required(true)
-                    .index(1),
-            ),
+            SubCommand::with_name("rm")
+                .about("Remove a given key")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("Key name")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::with_name("addr")
+                        .long("addr")
+                        .help("address to connect to server")
+                        .takes_value(true)
+                        .value_name("IP:PORT")
+                        .required(false),
+                ),
         )
         .get_matches();
 
     match matches.subcommand() {
         ("set", Some(matches)) => {
-            let key = matches.value_of("KEY").expect("KEY argument missing");
-            let value = matches.value_of("VALUE").expect("VALUE argument missing");
-
-            store.set(key.to_string(), value.to_string())?;
+            let addr = matches.value_of("addr").unwrap_or("127.0.0.1:4000");
+            TcpStream::connect(addr)?;
         }
         ("get", Some(matches)) => {
-            let key = matches.value_of("KEY").expect("KEY argument missing");
-            if let Some(value) = store.get(key.to_string())? {
-                println!("{}", value);
-            } else {
-                println!("Key not found");
-            }
+            let addr = matches.value_of("addr").unwrap_or("127.0.0.1:4000");
+            TcpStream::connect(addr)?;
         }
         ("rm", Some(matches)) => {
-            let key = matches.value_of("KEY").expect("KEY argument missing");
-            match store.remove(key.to_string()) {
-                Ok(()) => {}
-                Err(err) => {
-                    println!("{}", err);
-                    exit(1);
-                }
-            }
+            let addr = matches.value_of("addr").unwrap_or("127.0.0.1:4000");
+            TcpStream::connect(addr)?;
         }
         _ => {
-            eprintln!("command Not Found");
+            eprintln!("Command Not Found");
             exit(1)
         }
     }
--- kvs-client-server/src/bin/kvs-server.rs
+++ kvs-client-server/src/bin/kvs-server.rs
@@ -1,5 +1,25 @@
+use clap::{App, Arg};
 use kvs::Result;
 
 fn main() -> Result<()> {
+    let matches = App::new("kvs-client")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("addr")
+                .long("addr")
+                .help("address to listen")
+                .takes_value(true)
+                .value_name("IP:PORT"),
+        )
+        .arg(
+            Arg::with_name("engine")
+                .help("storage engine to use")
+                .long("engine")
+                .value_name("ENGINE-NAME")
+                .takes_value(true),
+        )
+        .get_matches();
     Ok(())
 }
