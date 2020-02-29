dbd7c8088771e69ada6669c9d681b84719057a16
hindenbug <manoj.mk27@gmail.com>
2020-02-29 18:25:08 +0100

    Add existing engine swapping check, test green

--- kvs-client-server/src/bin/kvs-server.rs
+++ kvs-client-server/src/bin/kvs-server.rs
@@ -1,10 +1,13 @@
 use clap::{App, Arg};
 use kvs::{KvStore, KvsEngine, KvsError, Result, Server, SledKvsEngine};
-use log::{info, LevelFilter};
+use log::{error, info, LevelFilter};
 use std::env;
+use std::fs;
+use std::process::exit;
 use std::str::FromStr;
 
 const DEFAULT_LISTENING_ADDRESS: &str = "127.0.0.1:4000";
+const CONFIG_FILENAME: &str = "engine";
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum Engine {
@@ -31,41 +34,61 @@ fn main() -> Result<()> {
 
     info!("Kvs server starting up.");
 
-    let matches = App::new("kvs-client")
+    let matches = App::new("kvs-server")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            Arg::with_name("addr")
-                .long("addr")
-                .help("address to listen")
-                .takes_value(true)
-                .value_name("IP:PORT"),
-        )
-        .arg(
             Arg::with_name("engine")
-                .help("storage engine to use")
                 .long("engine")
                 .value_name("ENGINE-NAME")
+                .possible_values(&["kvs", "sled"])
                 .case_insensitive(true)
+                .help("Specify engine [default: kvs]")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("addr")
+                .long("addr")
+                .value_name("IP-PORT")
+                .help("Specify listening address")
+                .default_value("127.0.0.1:4000")
                 .takes_value(true),
         )
         .get_matches();
 
+    let curr_dir = env::current_dir()?;
+    let engine_config = curr_dir.join(CONFIG_FILENAME);
     let addr: &str = matches
         .value_of("addr")
         .unwrap_or(DEFAULT_LISTENING_ADDRESS);
-    let engine: Engine = Engine::from_str(matches.value_of("engine").unwrap_or("kvs"))?;
+
+    let engine_option = matches.value_of("engine").unwrap_or("kvs");
+
+    let engine = Engine::from_str(engine_option)?;
+    let curr_engine = match previous_engine_type()? {
+        Some(prev_engine) => {
+            if engine != prev_engine {
+                exit(1);
+            } else {
+                prev_engine
+            }
+        }
+        None => engine,
+    };
 
     info!(
         "kvs-server version: {}, storage engine {:?}",
         env!("CARGO_PKG_VERSION"),
-        engine
+        curr_engine
     );
     info!("Listening on: {}", addr.clone());
-    match engine {
-        Engine::Kvs => start_server_with(addr, KvStore::open(env::current_dir()?)?),
-        Engine::Sled => start_server_with(addr, SledKvsEngine::open(env::current_dir()?)?),
+
+    fs::write(&engine_config, format!("{}", engine_option))?;
+
+    match curr_engine {
+        Engine::Kvs => start_server_with(addr, KvStore::open(curr_dir)?),
+        Engine::Sled => start_server_with(addr, SledKvsEngine::open(curr_dir)?),
     }
 }
 
@@ -73,4 +96,16 @@ fn start_server_with<E: KvsEngine>(addr: &str, engine: E) -> Result<()> {
     let mut server = Server::new(addr, engine);
     server.serve()?;
     Ok(())
+}
+
+fn previous_engine_type() -> Result<Option<Engine>> {
+    let engine_config_path = env::current_dir()?.join(CONFIG_FILENAME);
+    if !engine_config_path.exists() {
+        Ok(None)
+    } else {
+        match fs::read_to_string(engine_config_path) {
+            Ok(engine) => Ok(Some(Engine::from_str(&engine)?)),
+            Err(_) => Ok(None),
+        }
+    }
 }
